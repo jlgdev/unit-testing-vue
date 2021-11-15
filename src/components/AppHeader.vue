5f93280d51a1849afa515cec80d8eb8572b3ce4e
jlgdev <gdurand@jlgarchitects.com>
2021-11-15 09:58:38 -0600

    initial test

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,8 @@
+<script setup>
+let loggedIn = false;
+</script>
+<template>
+  <div>
+    <button v-show="loggedIn">Logout</button>
+  </div>
+</template>
---
+++ tests/unit/AppHeader.spec.js
@@ -0,0 +1,7 @@
+import AppHeader from "@/components/AppHeader";
+
+describe("AppHeader", () => {
+  test("If user is not logged in, do not show logout button", () => {
+    expect(true).toBe(true);
+  });
+});
